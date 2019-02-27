a50f6185a49341f975db6c8d6e2a336556308935
Angel Negrete <angeldiaz_n@hotmail.com>
2019-02-27 16:45:08 -0600

    Added sounds and fixes in prefabs

---
+++ Assets/Scripts/VidaMarciano.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaMarciano : MonoBehaviour
+{
+    public BossGenerator boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boss = GameObject.Find("BossGenerator").GetComponent<BossGenerator>();
+		boss.HealthBar.size = boss.Health / 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Kill")
+        {
+            boss.Health -= 10;
+            boss.HealthBar.size = boss.Health / 100f;
+            if(boss.Health == 0){
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/explosion_player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion_player : MonoBehaviour
+{
+   public GameObject explosion;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives.vida2 <= 0)
+        {
+            explosion.SetActive(true);
+            player.SetActive(false);
+            
+        }
+    }
+}
