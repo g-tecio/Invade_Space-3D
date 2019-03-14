3c757ed1b8088dd3485c3d2d6d52f8f160c71229
Angel Negrete <angeldiaz_n@hotmail.com>
2019-03-14 15:54:23 -0600

    added new assets, explotion, 3D models

---
+++ Assets/Scripts/Collider_Destruction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_Destruction : MonoBehaviour
+{
+    public float _timer = 0f;
+  public  Collider m_Collider;
+    public bool state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_Collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == true)
+        {
+            _timer += Time.deltaTime;
+           // print(_timer);
+            m_Collider.enabled = false;
+
+            if (_timer >= 3)
+            {
+                m_Collider.enabled = true;
+                state = false;
+                _timer = 0f;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    public CanvasGroup uiElement;
+    public Animator anim;
+    public bool muerte=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives.vida2 <= 0 && muerte== false)
+        {
+            updateState("FadeIn");
+            muerte = true;
+
+
+        }
+
+
+    }
+    public void FadeIn()
+    {
+        StartCoroutine(FadePanel(uiElement, uiElement.alpha, 1));
+    }
+    public IEnumerator FadePanel(CanvasGroup cg, float start, float end, float lerpTime = 0.2f)
+    {
+        float _timeStartedLerping = Time.time;
+        float timeSinceStarted = Time.time - _timeStartedLerping;
+        float percentageComplete = timeSinceStarted / lerpTime;
+        while (true)
+        {
+            timeSinceStarted = Time.time - _timeStartedLerping;
+            percentageComplete = timeSinceStarted / lerpTime;
+            float currentValue = Mathf.Lerp(start, end, percentageComplete);
+
+            cg.alpha = currentValue;
+            if (percentageComplete >= 1) break;
+
+            print("aqui");
+
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+
+    public void updateState(string state = null)
+    {
+        if (state != null)
+        {
+            anim.Play(state);
+        }
+    }
+}
